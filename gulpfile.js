78aaaa0891999e98bd70ec64cf9e8fe2f86434ca
wokeGit <abmelak@o2.pl>
2015-06-29 19:59:59 +0200

    server running moved to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,25 @@
 var gulp = require('gulp');
+var express = require('express'),
+	app 	= express();
+
+app.get('/', function(req, res) {
+	//console.log(__dirname + '\\app\\index.html')
+	res.sendFile(__dirname + '/app/index.html');
+});
 
 gulp.task('default', function() {
   // place code for your default task here
 });
+
+gulp.task('server', function(){
+
+	app.use('/app', express.static(__dirname + '/app'));
+	app.use('/node_modules', express.static(__dirname + '/node_modules'));
+
+	app.listen(3000, function() {
+		console.log('I\'m Listening...');
+	});
+
+});
+
+gulp.task('run', ['server']);
--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-var gulp = require('gulp');
-var express = require('express'),
-	app 	= express();
+// var gulp = require('gulp');
+// var express = require('express'),
+// 	app 	= express();
 
-gulp.task('default', function() {
-  // place code for your default task here
-});
+// gulp.task('default', function() {
+//   // place code for your default task here
+// });
 
-app.get('/', function(req, res) {
-	//console.log(__dirname + '\\app\\index.html')
-	res.sendFile(__dirname + '/app/index.html');
-});
+// app.get('/', function(req, res) {
+// 	//console.log(__dirname + '\\app\\index.html')
+// 	res.sendFile(__dirname + '/app/index.html');
+// });
 
-app.use('/app', express.static(__dirname + '/app'));
-app.use('/node_modules', express.static(__dirname + '/node_modules'));
+// app.use('/app', express.static(__dirname + '/app'));
+// app.use('/node_modules', express.static(__dirname + '/node_modules'));
 
-app.listen(3000, function() {
-	console.log('I\'m Listening...');
-});
+// app.listen(3000, function() {
+// 	console.log('I\'m Listening...');
+// });
